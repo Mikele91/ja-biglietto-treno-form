cbf31cf7912b31f27debf25fc997e80f4771e251
Mikele91 <michele.amoruso91.8@gmail.com>
2021-08-30 19:04:37 +0200

    fine parte logica

--- js/main.js
+++ js/main.js
@@ -14,26 +14,47 @@ function(){
     var fasciaEta = document.getElementById("fascia").value;
     
     //4. calcolo i prezzi
-    // if(fasciaEta == "minorenne"){
-    //     //prezzo biglietto -20%
-    // }else if (fasciaEta == "over"){
-    //     //prezzo biglietto -40%
-        
-    // }else {
-    //     // prezzo normale
-    // }
 
+    var prezzoBiglietto = parseInt(km*0.21).toFixed(2);
+    
+
+     if(fasciaEta == "minorenne"){
+        prezzoBiglietto -= (prezzoBiglietto*0.20);
+        console.log(prezzoBiglietto.toFixed(2));
+
+        //prezzo biglietto -20% 
+     }else if (fasciaEta == "over"){
+        prezzoBiglietto -= (prezzoBiglietto*0.40);
+        console.log(prezzoBiglietto.toFixed(2));
+
+         //prezzo biglietto -40%
+        
+    }
+
+    
     
     //5. stampo tutti i dati nel div del biglietto
     //5a. metto nome passeggiero
     document.getElementById("nome-passegiero").innerHTML = nome;
     //5b. metto offerta
-    document.getElementById("tipo-biglietto").innerHTML = fasciaEta;
+    if(fasciaEta == "minorenne"){
+    document.getElementById("tipo-biglietto").innerHTML = " Under 18";
+
+    }else if(fasciaEta == "over"){
+        document.getElementById("tipo-biglietto").innerHTML = " Over 65"
+    }else {
+        document.getElementById("tipo-biglietto").innerHTML = " Nessuno sconto"
+    }
+    
     //5c. inserisco prezzo
-    document.getElementById("totale").innerHTML = km;
+    document.getElementById("totale").innerHTML = prezzoBiglietto;
 
     //5d. inserisco carrozza e codice cp
     
+    var carrozza  = (Math.floor(Math.random() * (10-1)+1)) ;
+    console.log(carrozza);
+    var cp = carrozza.toString() +  (Math.floor(Math.random() *(10000-1000)+1000));
+    console.log(cp);
     // 6 mostra bigleitto
     document.getElementById("biglietto").classList.add("open");
 }
@@ -49,6 +70,6 @@ function(){
     // resetto i campi
     var nome = document.getElementById("nome").value = " ";
     var km = parseInt( document.getElementById("km").value = " ") ;
-    var fasciaEta = document.getElementById("fascia").value= " " ;
+    var fasciaEta = document.getElementById("fascia").value= " Seleziona la tua opzione" ;
 }
 )
